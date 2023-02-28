1983b23d53894da8e7247b9dbdf0f95eec28f501
silence_zhpf <silence_zhpf@aliyun.com>
2023-02-28 15:43:58 +0800

    chore: 🚧 get和post方法添加更多参数

--- src/RxAxios.ts
+++ src/RxAxios.ts
@@ -1,17 +1,27 @@
-import axios, { type AxiosResponse } from "axios";
+import axios, { type AxiosRequestConfig, type AxiosResponse } from "axios";
 import { from, type Observable, type ObservedValueOf } from "rxjs";
 
 type ObservableAxiosResponse<T> = Observable<
   ObservedValueOf<Promise<AxiosResponse<T>>>
 >;
 
+type GetRequestConfig<T> = Omit<AxiosRequestConfig<T>, "url" | "method">;
+type PostRequestConfig<T> = Omit<AxiosRequestConfig<T>, "url" | "method">;
+
 class RxAxios {
-  get<T = any>(url: string): ObservableAxiosResponse<T> {
-    return from(axios.get(url));
+  get<T = any>(
+    url: string,
+    config?: GetRequestConfig<any>
+  ): ObservableAxiosResponse<T> {
+    return from(axios.get(url, config));
   }
 
-  post<T = any>(url: string): ObservableAxiosResponse<T> {
-    return from(axios.post(url));
+  post<T = any>(
+    url: string,
+    data?: any,
+    config?: PostRequestConfig<any>
+  ): ObservableAxiosResponse<T> {
+    return from(axios.post(url, data, config));
   }
 }
 
