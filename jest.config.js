3aa8903209ad111cb713465309a019d6260f25d4
silence_zhpf <silence_zhpf@aliyun.com>
2023-03-02 13:28:21 +0800

    chore: 🔧 添加代码覆盖率设置

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,13 @@ module.exports = {
   moduleNameMapper: {
     "^@src/(.*)": "<rootDir>/src/$1",
   },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 };
