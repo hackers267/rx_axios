3fa84f37eb9d5ec372359e9f8e6a8e8295b8115c
silence_zhpf <silence_zhpf@aliyun.com>
2023-03-02 15:25:04 +0800

    chore: 🚧 添加多个打包格式

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,8 +5,32 @@ export default defineConfig([
   {
     input: "src/index.ts",
     output: {
+      format: "cjs",
       file: "dist/index.js",
     },
+    plugins: [typescript({})],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      format: "es",
+      file: "dist/index.esm.js",
+    },
+    external: ["axios", "rxjs"],
+    plugins: [typescript({})],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      format: "umd",
+      file: "dist/index.aio.js",
+      name: "rx_axios",
+      globals: {
+        axios: "axios",
+        rxjs: "rxjs",
+      },
+    },
+    external: ["axios", "rxjs"],
     plugins: [typescript()],
   },
 ]);
